7e1d1edec5d6cd6d696aabe74544c674d9de813a
indri345 <indri.nainggolan23@student.president.ac.id>
2026-06-26 09:18:27 +0700

    Update settings.py

--- digital_culture/settings.py
+++ digital_culture/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-g-t1)#iw*swcw^(1_e7fy31)!!901(-imt_beq^tzq6%j7xv4t
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
- 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -129,9 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# TAMBAHKAN BARIS INI:
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
