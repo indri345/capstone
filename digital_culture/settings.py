c4b5c0e0a8896dd6b86d238f2e0afaf6bd018dfa
indri345 <indri.nainggolan23@student.president.ac.id>
2026-06-26 07:39:32 +0700

    Update settings.py

--- digital_culture/settings.py
+++ digital_culture/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# TAMBAHKAN BARIS INI:
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
