b10f33547e6c57c9a8555714790707c0049608d8
indri345 <indri.nainggolan23@student.president.ac.id>
2026-06-26 09:08:36 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-# 1. Jalankan pengumpulan file statis
-python manage.py collectstatic --no-input
+# Jalankan migrasi database (opsional tapi disarankan)
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
-# 2. Jalankan migrasi database PostgreSQL
-python manage.py migrate --no-input
-
-# 3. Nyalakan server Gunicorn dengan port dinamis Railway
-exec gunicorn digital_culture.wsgi:application --bind 0.0.0.0:$PORT
+# Jalankan Gunicorn (Pastikan menggunakan tanda kutip ganda atau tanpa kutip untuk $PORT)
+exec gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT
